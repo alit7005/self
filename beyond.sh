764bfe3ecd2337babbbbf363202b47658466db3a
alit7005 <gemclash@yahoo.com>
2017-05-27 22:28:34 +0430

    Update beyond.sh

--- beyond.sh
+++ beyond.sh
@@ -25,20 +25,12 @@ install() {
 
 function print_logo() {
 	echo -e "\033[38;5;600m"
-	echo -e "          ____  ____     _____"
-	echo -e "         |  _ )|  _ \   |_   _|___ ____   __  __"
-	echo -e "         |  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-	echo -e "         |____/|____/     |_|\____/\_____|_/\/\_|"
 	echo -e "\n\e[36m"
 }
 
 function logo_play() {
     declare -A txtlogo
     seconds="0.010"
-    txtlogo[1]=" ____  ____     _____"
-    txtlogo[2]="|  _ )|  _ \   |_   _|___ ____   __  __"
-    txtlogo[3]="|  _ \| |_) )    | |/ .__|  _ \_|  \/  |"
-    txtlogo[4]="|____/|____/     |_|\____/\_____|_/\/\_|"
     printf "\033[38;5;600m\t"
     for i in ${!txtlogo[@]}; do
         for x in `seq 0 ${#txtlogo[$i]}`; do
@@ -63,11 +55,11 @@ if [ ! -f ./tg/tgcli ]; then
 
    print_logo
    echo -e "\033[38;5;208m"
-   echo -e "     >>>>                       We Are Not Attacker                             "
-   echo -e "     >>>>                       We Are Not Alliance                             "
-   echo -e "     >>>>                       We Are Family                                   "
+   echo -e "     >>>>                       We Are Attacker                             "
+   echo -e "     >>>>                       We Are Alliance                             "
+   echo -e "     >>>>                       We Are Cracker                                   "
    echo -e "     >>>>                       We Are The Best :-)                             "
-   echo -e "     >>>>                       @BeyondTeam                                     "
+   echo -e "     >>>>                       @alit7005                                     "
    echo -e "\033[0;00m"
    echo -e "\e[36m"
    logo_play
